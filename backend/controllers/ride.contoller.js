23a1749a48a28f5afd62a75b4e064b1346b54a91
testingFoo <joannakwinta136@gmail.com>
2026-04-18 18:57:32 +0200

    Create ride.contoller.js

---
+++ backend/controllers/ride.contoller.js
@@ -0,0 +1,43 @@
+const Ride = require("../models/Ride");
+const Driver = require("../models/Driver");
+const dispatch = require("../services/dispatch.service");
+
+exports.createRide = async (req, res) => {
+
+  const driver = await dispatch.findDriver(req.body.type);
+
+  const ride = await Ride.create({
+    ...req.body,
+    status: driver ? "ACCEPTED" : "REQUESTED",
+    driverId: driver ? driver._id : null,
+    fare: Math.floor(Math.random() * 30) + 10
+  });
+
+  if (driver) {
+    driver.available = false;
+    await driver.save();
+  }
+
+  req.io.emit("ride:new", ride);
+
+  res.json(ride);
+};
+
+exports.updateStatus = async (req, res) => {
+  const ride = await Ride.findById(req.params.id);
+
+  ride.status = req.body.status;
+
+  if (req.body.status === "COMPLETED") {
+    await Driver.findByIdAndUpdate(ride.driverId, {
+      available: true,
+      $inc: { completedTrips: 1 }
+    });
+  }
+
+  await ride.save();
+
+  req.io.emit("ride:update", ride);
+
+  res.json(ride);
+};
