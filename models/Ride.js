30605ca397779273bd91c4d90452929d455c7bea
testingFoo <joannakwinta136@gmail.com>
2026-04-18 18:50:34 +0200

    Create Ride.js

---
+++ models/Ride.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const RideSchema = new mongoose.Schema({
+  type: String, // PASSENGER | PARCEL | FREIGHT
+
+  origin: String,
+  destination: String,
+
+  originCoords: Object,
+  destinationCoords: Object,
+
+  status: {
+    type: String,
+    default: "REQUESTED"
+  },
+
+  riderId: String,
+  driverId: String,
+
+  fare: Number,
+
+  // freight fields
+  equipment: String,
+  weight: String,
+  distance: Number,
+  rate: Number
+});
+
+module.exports = mongoose.model("Ride", RideSchema);
