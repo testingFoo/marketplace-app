434420054dc135b4c8f9aaf2dc0b73aec16c18b7
testingFoo <joannakwinta136@gmail.com>
2026-04-18 19:14:30 +0200

    Update load.routes.js

--- backend/routes/load.routes.js
+++ backend/routes/load.routes.js
@@ -1,1 +1,8 @@
+const router = require("express").Router();
+const ctrl = require("../controllers/load.controller");
 
+router.get("/", ctrl.getLoads);
+router.post("/", ctrl.createLoad);
+router.patch("/:id/accept", ctrl.acceptLoad);
+
+module.exports = router;
