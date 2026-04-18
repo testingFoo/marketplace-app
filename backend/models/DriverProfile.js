86df8c00a98bb383c493ba5ee1fb4401497514ac
testingFoo <joannakwinta136@gmail.com>
2026-04-18 19:10:14 +0200

    Update DriverProfile.js

--- backend/models/DriverProfile.js
+++ backend/models/DriverProfile.js
@@ -1,1 +1,24 @@
+const mongoose = require("mongoose");
 
+const DriverSchema = new mongoose.Schema({
+  userId: String,
+
+  name: String,
+  car: String,
+  plate: String,
+
+  vehicleType: {
+    type: String,
+    enum: ["UberX", "Comfort", "Van", "LTL", "FTL"]
+  },
+
+  fleetCompany: String,
+  avatar: String,
+
+  rating: { type: Number, default: 4.8 },
+  completedTrips: { type: Number, default: 500 },
+
+  available: { type: Boolean, default: false }
+});
+
+module.exports = mongoose.model("DriverProfile", DriverSchema);
