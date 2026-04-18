cb1b4bc95b3840e6666126e3e898e027fa018558
testingFoo <joannakwinta136@gmail.com>
2026-04-18 19:13:38 +0200

    Update auth.middleware.js

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,1 +1,16 @@
 
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) return res.status(401).json("no token");
+
+  try {
+    const decoded = jwt.verify(token.split(" ")[1], "SECRET");
+    req.user = decoded;
+    next();
+  } catch (e) {
+    res.status(401).json("invalid token");
+  }
+};
