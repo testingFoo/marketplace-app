8e2edf451d3f958718f00ec5ea744a88d564882e
testingFoo <joannakwinta136@gmail.com>
2026-04-18 19:13:12 +0200

    Update load.controller.js

--- backend/controllers/load.controller.js
+++ backend/controllers/load.controller.js
@@ -1,1 +1,26 @@
 
+const Load = require("../models/Load");
+
+// CREATE LOAD (SHIPPER)
+exports.createLoad = async (req, res) => {
+  const load = await Load.create(req.body);
+  res.json(load);
+};
+
+// GET LOADBOARD
+exports.getLoads = async (req, res) => {
+  const loads = await Load.find();
+  res.json(loads);
+};
+
+// ACCEPT LOAD
+exports.acceptLoad = async (req, res) => {
+  const load = await Load.findById(req.params.id);
+
+  load.status = "BOOKED";
+  load.driverId = req.body.driverId;
+
+  await load.save();
+
+  res.json(load);
+};
