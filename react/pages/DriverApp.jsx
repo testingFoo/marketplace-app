6bd1ad9bdd8a8dd4446545a842ad5a79c4ff8d51
testingFoo <joannakwinta136@gmail.com>
2026-04-20 20:25:45 +0200

    Update DriverApp.jsx

--- react/pages/DriverApp.jsx
+++ react/pages/DriverApp.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
-import { API } from "../api/api";
 import { socket } from "../socket/socket";
+import { API } from "../api/api";
+
+import DriverJobs from "../components/DriverJobs";
 
 export default function DriverApp() {
   const [rides, setRides] = useState([]);
-  const driverId = "D-" + Math.floor(Math.random() * 99999);
 
   useEffect(() => {
     loadJobs();
@@ -19,31 +20,12 @@ export default function DriverApp() {
     setRides(data);
   }
 
-  async function acceptRide(id) {
-    await fetch(`${API}/api/rides/${id}/accept`, {
-      method: "PATCH",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ driverId })
-    });
-
-    loadJobs();
-  }
-
   return (
     <div style={{ display: "flex" }}>
       <div style={{ width: 400, background: "#111", color: "white" }}>
         <h2>Driver Panel</h2>
 
-        {rides
-          .filter(r => r.status === "REQUESTED")
-          .map(r => (
-            <div key={r._id} style={{ margin: 10 }}>
-              <b>{r.type}</b>
-              <button onClick={() => acceptRide(r._id)}>
-                Accept
-              </button>
-            </div>
-          ))}
+        <DriverJobs rides={rides} reload={loadJobs} />
       </div>
     </div>
   );
