20d36b692450cbe94ee1159ea0eda81831087060
testingFoo <joannakwinta136@gmail.com>
2026-04-20 18:17:39 +0200

    Update geo.service.js

--- backend/services/geo.service.js
+++ backend/services/geo.service.js
@@ -1,1 +1,23 @@
+exports.calculateDistance = (lat1, lon1, lat2, lon2) => {
+  if (!lat1 || !lon1 || !lat2 || !lon2) return 999;
 
+  const R = 6371;
+
+  const dLat = toRad(lat2 - lat1);
+  const dLon = toRad(lon2 - lon1);
+
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(toRad(lat1)) *
+    Math.cos(toRad(lat2)) *
+    Math.sin(dLon / 2) *
+    Math.sin(dLon / 2);
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  return R * c;
+};
+
+function toRad(v) {
+  return (v * Math.PI) / 180;
+}
