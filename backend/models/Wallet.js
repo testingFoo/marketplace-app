6bb1dd29b6adfb1a646bb2665c1166ba15813c66
testingFoo <joannakwinta136@gmail.com>
2026-04-22 21:02:53 +0200

    Update Wallet.js

--- backend/models/Wallet.js
+++ backend/models/Wallet.js
@@ -1,21 +1,39 @@
-
 const mongoose = require("mongoose");
 
 const WalletSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "User"
+    ref: "User",
+    required: true
   },
 
   balance: {
     type: Number,
-    default: 0
+    default: 10 // starting bonus
   },
 
   currency: {
     type: String,
     default: "USD"
-  }
+  },
+
+  transactions: [
+    {
+      type: {
+        type: String, // "ride", "purchase", "deposit", "withdraw"
+        required: true
+      },
+
+      amount: Number,
+
+      meta: Object,
+
+      createdAt: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ]
 });
 
 module.exports = mongoose.model("Wallet", WalletSchema);
