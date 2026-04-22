126f22dc47e7e8c7995c097d1283ebb7c52ac87d
testingFoo <joannakwinta136@gmail.com>
2026-04-22 17:34:51 +0200

    Update passport.js

--- backend/auth/passport.js
+++ backend/auth/passport.js
@@ -1,37 +1,23 @@
-// auth/passport.js
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-const bcrypt = require("bcrypt");
-
+const { Strategy: JwtStrategy, ExtractJwt } = require("passport-jwt");
 const User = require("../models/User");
 
-passport.use(
-  new LocalStrategy(
-    { usernameField: "email" },
-    async (email, password, done) => {
+module.exports = (passport) => {
+  const options = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: "SECRET"
+  };
+
+  passport.use(
+    new JwtStrategy(options, async (payload, done) => {
       try {
-        const user = await User.findOne({ email });
-        if (!user) return done(null, false);
+        const user = await User.findById(payload.id);
 
-        const match = await bcrypt.compare(password, user.password);
-        if (!match) return done(null, false);
-
-        return done(null, user);
+        if (user) return done(null, user);
+        return done(null, false);
 
       } catch (err) {
-        return done(err);
+        return done(err, false);
       }
-    }
-  )
-);
-
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser(async (id, done) => {
-  const user = await User.findById(id);
-  done(null, user);
-});
-
-module.exports = passport;
+    })
+  );
+};
