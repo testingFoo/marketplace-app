b088cf73124fe52d6c70ee2830aa661d61c9e2be
testingFoo <joannakwinta136@gmail.com>
2026-04-22 20:58:16 +0200

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,13 +4,25 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, unique: true },
   password: String,
   name: String,
-
+  dob: Date,
+  sex: { type: String, enum: ["male", "female", "other"] },
+education: {
+    type: String},
+  currentCity: String,
+  bornCity: String,
+  profilePic: String,
   roles: {
     driver: { type: Boolean, default: false },
-    business: { type: Boolean, default: false }
+   business: {
+    isBusinessOwner: { type: Boolean, default: false },
+    name: String,
+    website: String,
+    email: String
+  },
   },
 
-  walletBalance: { type: Number, default: 0 },
+  walletBalance: { type: Number, default: 10 },
+  currency: { type: String, default: "USD" },
 
   createdAt: {
     type: Date,
