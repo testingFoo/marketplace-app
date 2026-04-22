861f454fc8358041be7b4d7dca067b612eeba0e7
testingFoo <joannakwinta136@gmail.com>
2026-04-22 16:40:48 +0200

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,10 +1,21 @@
+// models/User.js
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  role: { type: String, enum: ["rider", "driver"], required: true },
-  email: String,
+  email: { type: String, unique: true },
   password: String,
-  name: String
+
+  name: String,
+
+  roles: {
+    type: [String],
+    default: ["USER"] // can add DRIVER, BUSINESS_OWNER later
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model("User", UserSchema);
