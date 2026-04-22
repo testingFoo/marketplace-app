7b7bbf38bdf4790b2e1010ad48a44bc73d626e66
testingFoo <joannakwinta136@gmail.com>
2026-04-22 16:58:30 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,14 @@ app.use(express.static("public"));
 // ================= MIDDLEWARE =================
 app.use(cors());
 app.use(express.json());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(session({
+  secret: "super-secret",
+  resave: false,
+  saveUninitialized: false
+}));
+
 
 // 🔥 REQUEST LOGGER
 app.use((req, res, next) => {
