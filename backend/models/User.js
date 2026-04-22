08caa394aee99932c79dd7fe014792ddf45e28a5
testingFoo <joannakwinta136@gmail.com>
2026-04-22 17:44:23 +0200

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,16 +3,14 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   email: { type: String, unique: true },
   password: String,
-
   name: String,
-
-  // 🔥 SUPER APP FIELDS
-  walletBalance: { type: Number, default: 0 },
 
   roles: {
     driver: { type: Boolean, default: false },
     business: { type: Boolean, default: false }
   },
+
+  walletBalance: { type: Number, default: 0 },
 
   createdAt: {
     type: Date,
