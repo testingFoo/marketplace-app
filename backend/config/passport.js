3ce8eace085ac877499addeb942202d74cb910ea
testingFoo <joannakwinta136@gmail.com>
2026-04-22 17:31:43 +0200

    Update passport.js

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -1,5 +1,4 @@
 const { Strategy: JwtStrategy, ExtractJwt } = require("passport-jwt");
-const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcryptjs");
 
 const User = require("../models/User");
@@ -27,25 +26,3 @@ module.exports = (passport) => {
     })
   );
 
-  // ================= LOCAL LOGIN STRATEGY (NEW, SAFE) =================
-  passport.use(
-    new LocalStrategy(
-      { usernameField: "email" },
-      async (email, password, done) => {
-        try {
-          const user = await User.findOne({ email });
-
-          if (!user) return done(null, false);
-
-          const match = await bcrypt.compare(password, user.password);
-          if (!match) return done(null, false);
-
-          return done(null, user);
-
-        } catch (err) {
-          return done(err);
-        }
-      }
-    )
-  );
-};
