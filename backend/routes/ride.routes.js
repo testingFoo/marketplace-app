59138f1befe5930f340905a116af99fcf1f21b41
testingFoo <joannakwinta136@gmail.com>
2026-04-22 11:23:09 +0200

    Update ride.routes.js

--- backend/routes/ride.routes.js
+++ backend/routes/ride.routes.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
-
+const driverCtrl = require("../controllers/driverActions.controller");
 const ctrl = require("../controllers/ride.controller");
 
 // BASIC FLOW ONLY (SAFE)
 router.get("/", ctrl.getRides);
 router.post("/", ctrl.createRide);
 router.patch("/:id/accept", ctrl.acceptRide);
+router.patch("/:id/start-to-pickup", driverCtrl.startToPickup);
+router.patch("/:id/arrived", driverCtrl.arrivedAtPickup);
+router.patch("/:id/start-trip", driverCtrl.startTrip);
 
 module.exports = router;
