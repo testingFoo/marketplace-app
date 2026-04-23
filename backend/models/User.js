92ef7d2ff2ebcd8b091606acf159d21ed368e650
testingFoo <joannakwinta136@gmail.com>
2026-04-23 12:13:27 +0200

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,29 +1,84 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
+  
   email: { type: String, unique: true },
   password: String,
-  name: String,
-  dob: Date,
+  
+  // ================= BASIC PROFILE =================
+  firstName: String,
+  lastName: String,
   sex: { type: String, enum: ["male", "female", "other"] },
-education: {
+  dob: Date,
+  phone: {
+    countryCode: String,
+    number: String
+  },
+  education: {
     type: String},
   currentCity: String,
   bornCity: String,
   profilePic: String,
-  roles: {
-    driver: { type: Boolean, default: false },
-   business: {
-    isBusinessOwner: { type: Boolean, default: false },
+  
+  // ================= LOCATION =================
+  city: String,
+  country: String,
+
+   // ================= PROFESSIONAL =================
+  profession: String,
+  industry: {
+    type: String,
+    enum: [
+      "Technology",
+      "Finance",
+      "Healthcare",
+      "Education",
+      "Construction",
+      "Transport",
+      "Retail",
+      "Manufacturing",
+      "Agriculture",
+      "Media",
+      "Legal",
+      "Other"
+    ]
+  },
+  
+  // ================= BUSINESS =================
+  
+  business: {
+    isOwner: { type: Boolean, default: false },
     name: String,
     website: String,
-    email: String
-  },
+    email: String,
+    social: {
+      instagram: String,
+      linkedin: String,
+      youtube: String
+    }
   },
 
+  // ================= ASSETS / COMMODITIES =================
+  commodities: [
+    {
+      type: {
+        type: String,
+        enum: ["grain", "metal", "other"]
+      },
+      name: String,   // e.g. Wheat, Gold
+      quantity: Number
+    }
+  ],
+
+// ================= WALLET =================
   walletBalance: { type: Number, default: 10 },
   currency: { type: String, default: "USD" },
 
+   roles: {
+    driver: { type: Boolean, default: false },
+   },
+
+  
   createdAt: {
     type: Date,
     default: Date.now
