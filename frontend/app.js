2d37dc3163e66bc08b47758ef29d9ec7b73c682c
testingFoo <joannakwinta136@gmail.com>
2026-04-23 19:48:39 +0200

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -3,17 +3,13 @@ const API = "https://marketplace-app-m8ac.onrender.com";
 // ================= REGISTER =================
 async function register() {
 
-    const firstName = document.getElementById("firstName")?.value?.trim()
-    const lastName = document.getElementById("lastName")?.value?.trim()
     const email = document.getElementById("email")?.value?.trim()
     const password = document.getElementById("password")?.value
     
     console.log("EMAIL:", email);
     console.log("PASSWORD:", password);
-    console.log("Last Name", lastName);
-    console.log("FIrst Name", firstName);
     
-    const payload = { firstName, lastName, email, password};
+    const payload = { email, password};
 
   try {
     const res = await fetch(`${API}/api/auth/register`, {
