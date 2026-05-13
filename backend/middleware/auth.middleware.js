61c0d49650bb3365a792d3733e6edeee40e05e46
testingFoo <joannakwinta136@gmail.com>
2026-05-13 13:51:24 +0200

    Update auth.middleware.js

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -11,7 +11,7 @@ module.exports = async (req, res, next) => {
 
     const token = header.split(" ")[1];
 
-    const decoded = jwt.verify(token, "SECRET");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     const user = await User.findById(decoded.id).select("-password");
 
@@ -19,20 +19,9 @@ module.exports = async (req, res, next) => {
       return res.status(401).json({ error: "user not found" });
     }
 
-    req.user = {
-      id: user._id,
-      email: user.email,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      currentCity: user.currentCity,
-      country: user.country,
-      hasBusiness: user.hasBusiness || false
-    };
-
-    req.userFull = user;
+    req.user = user;
 
     next();
-
   } catch (err) {
     return res.status(401).json({ error: "invalid token" });
   }
