ebde0cb0bc0356f5e30b6da01aeb69a704bc203a
testingFoo <joannakwinta136@gmail.com>
2026-05-13 19:43:36 +0200

    Update Event.js

--- backend/models/Event.js
+++ backend/models/Event.js
@@ -1,54 +1,56 @@
 const mongoose = require("mongoose");
 
 const EventSchema = new mongoose.Schema({
-
   type: {
     type: String,
     required: true,
-    index: true
+    index: true,
   },
 
   severity: {
     type: Number,
-    default: 1
+    default: 1,
   },
 
   location: {
     lat: Number,
     lng: Number,
-    radius: Number
+    radius: Number,
   },
 
   time: {
     start: {
       type: Date,
-      default: Date.now
+      default: Date.now,
     },
-    end: Date
+
+    end: Date,
   },
 
   source: {
     type: String,
     default: "system",
-    index: true
+    index: true,
   },
 
   data: {
     type: Object,
-    default: {}
+    default: {},
   },
 
   createdAt: {
     type: Date,
     default: Date.now,
-    index: true
-  }
-
+    index: true,
+  },
 });
 
 // ✅ IMPORTANT FOR NASA DUPLICATE CHECKS
 EventSchema.index({
-  "data.eonetId": 1
+  "data.eonetId": 1,
 });
 
-module.exports = mongoose.model("Event", EventSchema);
+module.exports = mongoose.model(
+  "Event",
+  EventSchema
+);
